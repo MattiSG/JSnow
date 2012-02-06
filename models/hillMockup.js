cfc084fc1672c1a2d359213d4c644a6f9521a094
Matti Schneider-Ghibaudo <matti.schneiderghibaudo@gmail.com>
2012-02-06 21:24:27 +0100

    Refactored file hierarchy to highlight MVC architecture and routing.

---
+++ models/hillMockup.js
@@ -0,0 +1,22 @@
+exports.auron = {
+	name: "Auron",
+	mark: 3.4,
+	runs: {
+		green: {
+			open: 4,
+			total: 5
+		},
+		blue: {
+			open: 6,
+			total: 8
+		},
+		red: {
+			open: 7,
+			total: 10
+		},
+		black: {
+			open: 2,
+			total: 3
+		}
+	}
+}
