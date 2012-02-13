c6ec81d3447c50313ecb1af562e17061bdccebf2
jeremy <jeremy.gabriele@gmail.com>
2012-02-13 16:18:05 +0100

    registration now works.

---
+++ models/user.js
@@ -0,0 +1,41 @@
+var mongoose      = require('mongoose'),
+    Schema        = mongoose.Schema,
+    mongooseAuth  = require('mongoose-auth');
+
+var UserSchema = new Schema({
+  name:String,
+  lastname:String
+});
+
+UserSchema.plugin(mongooseAuth, {
+    everymodule: {
+      everyauth: {
+        User: function () {
+          return User;
+        }
+      }
+    },
+    password: {
+      loginWith: 'email', //loginWith: 'phone' also possible
+      
+      extraParams:{
+        name:{
+          first:String,
+          last:String
+        }
+      },
+
+      everyauth: {
+            getLoginPath: '/login'
+          , postLoginPath: '/login'
+          , loginView: 'login.ejs'
+          , getRegisterPath: '/register'
+          , postRegisterPath: '/register'
+          , registerView: 'register.ejs'
+          , loginSuccessRedirect: '/'
+          , registerSuccessRedirect: '/'
+        }
+    }
+});
+
+mongoose.model('User', UserSchema);
