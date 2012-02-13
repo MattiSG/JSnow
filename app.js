af089d7d9b09ba76600df88066375d9e6bfb96b3
Matti Schneider-Ghibaudo <matti.schneiderghibaudo@gmail.com>
2012-02-13 15:57:55 +0100

    Improved routes handling.

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
  */
 
 var express = require('express'),
-	routes = require('./routes');
+	routes = require('./routes'),
+	mootools = require('./lib/mootools-core');
 
 var app = module.exports = express.createServer();
 
@@ -32,17 +33,11 @@ app.configure('production', function(){
 	app.use(express.errorHandler()); 
 });
 
-// Routes
-
-app.get('/', routes.login);
-
-//hills'
-app.get('/hills/new', routes.hills_new); // pb if we put this line after /hills/:id
-app.post('/hills/new', routes.hills_create); // same route as app.get ?
-app.get('/hills/update/:hillName', routes.hills_update);
-app.get('/hills/:hillName', routes.hills);
-app.get('/hills', routes.hills);
-
+Object.each(routes, function(mappings, method) {
+	Object.each(mappings, function(handler, pattern) {
+		app[method](pattern, handler);
+	});
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes.js
+++ routes.js
@@ -1,45 +1,35 @@
-
-// users
-
-exports.register = function(req, res) {
-	res.render('users/register');
-};
-
-exports.login = function(req, res) {
-  // if user is not already logged in
-	res.render('users/login');
-	// else res.render('hills/view', require('./controllers/hill'));
-};
-
-
-// hills
-
 var hillsController = require('./controllers/hill');
 
-exports.hills = function(req, res) {
-	if (req.params.hillName) {
-		res.render('hills/view', {
-			hills: [hillsController.hill(req.params.hillName)]
-    });
-	} else {
-		res.render('hills/view', {
-			hills: hillsController.hills()
-		});
+exports.get = {
+	'/': function(req, res) {
+		// if user is not already logged in
+		res.render('users/login');
+		// else res.render('hills/view', require('./controllers/hill'));
+	},
+	
+	'/hills/new': function(req, res) {
+		res.render('hills/new');
+	},
+	
+	'/hills/update/:hillName': function(req, res) {
+		if (req.params.hillName) {
+			res.render('hills/update', {
+				hill: hillsController.hill(req.params.hillName)
+			});
+		}
+	},
+	
+	'/hills/:hillName': function(req, res) {
+		res.render('hills/view', { hills: [hillsController.hill(req.params.hillName)] });
+	},
+	
+	'/hills': function(req, res) {
+		res.render('hills/view', { hills: hillsController.hills() });
 	}
-};
+}
 
-exports.hills_new = function(req, res) {
-	res.render('hills/new');
-};
-
-exports.hills_create = function(req, res) {
-	console.log(req.body); // infos du post
-};
-
-exports.hills_update = function(req, res) {
-  if (req.params.hillName) {
-    res.render('hills/update', {
-			hill: hillsController.hill(req.params.hillName)
-    });
-  }
-};
+exports.post = {
+	'/hills/new': function(req, res) {
+		res.render('hills/new');
+	}
+}
