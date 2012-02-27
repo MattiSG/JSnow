83f2b53be0ed2595695c6250c9f51987f5908732
jeremy <jeremy.gabriele@gmail.com>
2012-02-27 22:39:28 +0100

    Code commented and branded on my side ! ;)

--- controllers/hillController.js
+++ controllers/hillController.js
@@ -1,12 +1,13 @@
+/* hill controller. Provides all method to access, update and create hills
+*@author	Jeremy Gabriele
+*/
 var mongoose = require('mongoose'),
 	sockets = require('./sockets');
 
 var Hill = mongoose.model('Hill');
 var Comment = mongoose.model('Comment');
 
-var UserController = require('./userController');
-
-var nbAccess = 0;
+var nbAccess = 0; // total number of pages hits (when user get to a page, this number gets increase by 1)
 
 //TODO: these variables should be refactored in a config module
 var COMMENT_TIMEOUT = 3 * 60 * 1000,	// how long (in ms) before a comment is considered old (a candidate for pruning)
@@ -63,6 +64,7 @@ function updateAverages(hills) {
 	});
 }
 
+// unflatten transform object with properties like obj["a.b.c"] in obj[a][b][c]
 function unflatten(from) {
 	var to = {};
 	var elem = "", lastElem = null, before = null;
@@ -88,21 +90,22 @@ function unflatten(from) {
 
 exports.update = function(req, res) {
 	
-	var newHillValues = unflatten(req.body);
+	var newHillValues = unflatten(req.body); // req.body has all POST values
 	newHillValues.name = req.params.hillName;
 	
-	// we don't want to lose total values
+	// newHillValues.runs only contains the "open" values (from POST values)
+	// we don't want to lose all the "total" values if we update the whole "runs" property.
 	Hill.findOne({name: newHillValues.name}, function(err, doc){
 		if (err) throw err;
 		var runs = doc.runs;
 		var colors = ['green', 'blue', 'red', 'black'];
 		colors.each(function(color){
 			if (newHillValues.runs[color].open)
-				newHillValues.runs[color].total = runs[color].total;
+				newHillValues.runs[color].total = runs[color].total; // backup "total" property
 		});
 		
 		if (newHillValues.lifts.open)
-			newHillValues.lifts.total = doc.lifts.total;
+			newHillValues.lifts.total = doc.lifts.total; // ... same for lifts
 			
 		newHillValues.lastUpdate = new Date();
 		
@@ -120,14 +123,12 @@ exports.update = function(req, res) {
 }
 
 exports.updateForm = function(req, res) {
-	
 	Hill.findOne({name: req.params.hillName}, function(err,doc){
 		if (!err) res.render('hills/update', { hill: doc });
 	});
 }
 
 exports.viewAll = function(req, res) {
-
 	Hill.find({}, function(err,docs){
 		if (!err) {
 			updateAverages(docs);
@@ -137,7 +138,6 @@ exports.viewAll = function(req, res) {
 }
 
 exports.viewHill = function(req, res) {
-	
 	Hill.findOne({name: req.params.hillName}, function(err,doc){
 		if (!err) {
 			updateAverages([ doc ]);
@@ -192,20 +192,19 @@ exports.newComment = function(req, res) {
 	});
 	
 	comment.tags = [];
-	
+	// we iterate over tag list from POST values
 	for (var tagKey in POSSIBLE_TAGS)
 		if (POSSIBLE_TAGS.hasOwnProperty(tagKey))
 			if (from[POSSIBLE_TAGS[tagKey]])
 				comment.tags.push(POSSIBLE_TAGS[tagKey]);
 	
 	comment.date = new Date();
-	comment.expires = new Date();
-	comment.expires.setTime(comment.date.getTime() + COMMENT_TIMEOUT);
 	
 	comment.author = req.user
 					 ? req.user.firstName + " " + req.user.lastName
 					 : 'Anonyme';
 	
+	// update hill's comments
 	Hill.findOne({name: req.params.hillName}, function(err, doc) {
 		var newCommentList = doc.comments;
 		newCommentList.push(comment);
