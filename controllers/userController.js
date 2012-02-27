87d1687919d1e41b7de96f7ae6f5959a37a7ffdc
Matti Schneider-Ghibaudo <matti.schneiderghibaudo@gmail.com>
2012-02-27 18:18:11 +0100

    Whitespacing & error handling in userController refactor.

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,15 +3,17 @@ var mongoose = require('mongoose');
 var User = mongoose.model('User');
 
 exports.isAuthorized = function(req, res, next) {
-	if (req.user&&req.user.skimaster) {
+	if (req.user && req.user.skimaster) {
 		next();
 	} else {
-		req.flash('error', 'Vous n\'êtes pas un maître de ski !');
+		req.flash('error', "Vous n'êtes pas un maître de ski !");
 		res.redirect('/hills');	
 	}
 }
 
 exports.findUserByID = function(id) {
-	User.findOne({"_id": id}, function(err, user) {
+	User.findOne({ "_id": id }, function(err, user) {
+		if (err)
+			throw err;
 	});
 }
